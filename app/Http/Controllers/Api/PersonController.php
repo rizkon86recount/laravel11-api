fd1547c24a5b1d551835bfca5b9d2e45fd92170e
rizkon86system <rizkon86system@gmail.com>
2025-02-22 14:34:27 +0700

    update final

--- app/Http/Controllers/Api/PersonController.php
+++ app/Http/Controllers/Api/PersonController.php
@@ -14,7 +14,12 @@ class PersonController extends Controller
      */
     public function index()
     {
-        //
+        $person = Person::all();
+        return response()->json([
+            'status' => true,
+            'message' => 'Data berhasil ditemukan',
+            'data' => $person
+        ]);
     }
 
     /**
@@ -38,7 +43,7 @@ class PersonController extends Controller
         $person = Person::create($request->all());
         return response()->json([
             'status' => true,
-            'message' => 'data berhasil dimasukkan',
+            'message' => 'Data berhasil dimasukkan',
             'data' => $person
         ], 201);
     }
@@ -48,15 +53,38 @@ class PersonController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $person = Person::findOrFail($id);
+        return response()->json([
+            'status' => true,
+            'message' => 'Data berhasil ditemukan',
+            'data' => $person
+        ]);
     }
-
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'validasi error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $person = Person::findOrFail($id);
+        $person->update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Data berhasil diperbaiki',
+            'data' => $person
+        ], 200);
     }
 
     /**
@@ -64,6 +92,11 @@ class PersonController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $person = Person::findOrFail($id);
+        $person->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Data berhasil dihapus'
+        ], 204);
     }
 }
